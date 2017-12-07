baca6a1c298cf9b6975fdcd1308c9615d306e00c
Buğra Ekuklu <ekuklu@icloud.com>
2017-12-07 21:47:31 +0300

    Utility function for injecting scope validators.

---
+++ src/utils/injectScopeValidator.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+
+class InvalidScopeError extends Error { }
+
+/**
+ * Injects a validator routine for given scopes. Scope `'all'` would be
+ * included as a default to contain all of the scopes.
+ *
+ * `injectScopeValidator(fetchUsers, ['some', 'valid', 'scope', 'values'])`
+ *
+ * @throws InvalidScopeError
+ * @throws TypeError
+ */
+export default function injectScopeValidator(fn, scopes) {
+	if (scopes.indexOf('all') === -1) {
+		scopes.push('all');
+	}
+
+	return function (args) {
+		let providedScopes = arguments[arguments.length - 1];
+
+		if (providedScopes === '') {
+			throw new InvalidScopeError('no scope element provided');
+		} else if (_.isString(providedScopes)) {
+			providedScopes = [providedScopes];
+		} else if (!_.isArray(providedScopes)) {
+			throw new TypeError('scope is not a string or array');
+		} else if (providedScopes.length === 0) {
+			throw new InvalidScopeError('no scope element provided');
+		}
+
+		providedScopes
+		.map(el => {
+			return el.trim();
+		})
+		.some(el => {
+			if (scopes.indexOf(el) === -1) {
+				throw new InvalidScopeError(`scope element ${el} is not supported`);
+			}
+		});
+	};
+}
---
+++ tests/unit/utils/injectScopeValidator.js
@@ -0,0 +1,87 @@
+import { spy } from 'sinon';
+
+import injectScopeValidator from 'src/utils/injectScopeValidator';
+
+//  The default scopes provided to the injector
+const defaultScopes = ['another', 'wombat'];
+const allScope = ['all'];
+const validScope = ['another'];
+const invalidScope = ['catdog'];
+
+describe('injectScopeValidator', () => {
+	describe('scope argument as array', () => {
+		it('should noop if scope arguments of provided function is valid', () => {
+	    const func = spy();
+	    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+
+	    expect(() => { 
+	    	wrappedFunc(1, validScope);
+	    }).to.not.throw();
+	  });
+
+	  it('should throw error if there is an unsupported scope argument', () => {
+	  	const func = spy();
+	  	const wrappedFunc = injectScopeValidator(func, defaultScopes);
+
+	  	expect(() => { 
+	  		wrappedFunc(1, invalidScope);
+	  	}).to.throw();
+	  });
+
+	  it('should throw error if scope argument is empty array', () => {
+	  	const func = spy();
+	  	const wrappedFunc = injectScopeValidator(func, defaultScopes);
+
+	  	expect(() => { 
+	  		wrappedFunc(1, []);
+	  	}).to.throw();
+	  });
+
+	  it('should not throw error if scope argument is all but it had not been provided', () => {
+	  	const func = spy();
+	  	const wrappedFunc = injectScopeValidator(func, defaultScopes);
+
+	  	expect(() => { 
+	  		wrappedFunc(1, ['all']);
+	  	}).to.not.throw();
+	  });
+	});
+
+	describe('scope argument as a string', () => {
+		it('should noop if scope argument is valid', () => {
+	  	const func = spy();
+	    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+
+	    expect(() => { 
+	    	wrappedFunc(1, validScope[0]);
+	    }).to.not.throw();
+	  });
+
+	  it('should throw error if scope argument is unsupported', () => {
+	  	const func = spy();
+	    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+
+	    expect(() => { 
+	    	wrappedFunc(1, invalidScope[0]);
+	    }).to.throw();
+	  });	  
+
+	  it('should throw error if empty string is provided as a scope', () => {
+	  	const func = spy();
+	    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+
+	    expect(() => { 
+	    	wrappedFunc(1, '');
+	    }).to.throw();
+	  });
+	});
+
+	it('should throw error if scope argument is not an array or string', () => {
+  	const func = spy();
+    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+
+    expect(() => { 
+    	wrappedFunc(1, new Object({}));
+    }).to.throw(TypeError);
+  });
+});

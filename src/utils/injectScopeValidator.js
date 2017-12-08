e89a98073262db6fd7fdb5f89770ed16cd624e99
Buğra Ekuklu <ekuklu@icloud.com>
2017-12-08 14:15:14 +0300

    Converted tabs to spaces.

--- src/utils/injectScopeValidator.js
+++ src/utils/injectScopeValidator.js
@@ -12,31 +12,31 @@ class InvalidScopeError extends Error { }
  * @throws TypeError
  */
 export default function injectScopeValidator(fn, scopes) {
-	if (scopes.indexOf('all') === -1) {
-		scopes.push('all');
-	}
+  if (scopes.indexOf('all') === -1) {
+    scopes.push('all');
+  }
 
-	return function (args) {
-		let providedScopes = arguments[arguments.length - 1];
+  return function () {
+    let providedScopes = arguments[arguments.length - 1];
 
-		if (providedScopes === '') {
-			throw new InvalidScopeError('no scope element provided');
-		} else if (_.isString(providedScopes)) {
-			providedScopes = [providedScopes];
-		} else if (!_.isArray(providedScopes)) {
-			throw new TypeError('scope is not a string or array');
-		} else if (providedScopes.length === 0) {
-			throw new InvalidScopeError('no scope element provided');
-		}
+    if (providedScopes === '') {
+      throw new InvalidScopeError('no scope element provided');
+    } else if (_.isString(providedScopes)) {
+      providedScopes = [providedScopes];
+    } else if (!_.isArray(providedScopes)) {
+      throw new TypeError('scope is not a string or array');
+    } else if (providedScopes.length === 0) {
+      throw new InvalidScopeError('no scope element provided');
+    }
 
-		providedScopes
-		.map(el => {
-			return el.trim();
-		})
-		.some(el => {
-			if (scopes.indexOf(el) === -1) {
-				throw new InvalidScopeError(`scope element ${el} is not supported`);
-			}
-		});
-	};
+    providedScopes
+    .map(el => {
+      return el.trim();
+    })
+    .some(el => {
+      if (scopes.indexOf(el) === -1) {
+        throw new InvalidScopeError(`scope element ${el} is not supported`);
+      }
+    });
+  };
 }
--- tests/unit/utils/injectScopeValidator.js
+++ tests/unit/utils/injectScopeValidator.js
@@ -9,79 +9,81 @@ const validScope = ['another'];
 const invalidScope = ['catdog'];
 
 describe('injectScopeValidator', () => {
-	describe('scope argument as array', () => {
-		it('should noop if scope arguments of provided function is valid', () => {
-	    const func = spy();
-	    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+  describe('scope argument as array', () => {
+    it('should noop if scope arguments of provided function is valid', () => {
+      const func = spy();
+      const wrappedFunc = injectScopeValidator(func, defaultScopes);
 
-	    expect(() => { 
-	    	wrappedFunc(1, validScope);
-	    }).to.not.throw();
-	  });
+      expect(() => {
+        wrappedFunc(1, validScope);
+      }).to.not.throw();
+    });
 
-	  it('should throw error if there is an unsupported scope argument', () => {
-	  	const func = spy();
-	  	const wrappedFunc = injectScopeValidator(func, defaultScopes);
+    it('should throw error if there is an unsupported scope argument', () => {
+      const func = spy();
+      const wrappedFunc = injectScopeValidator(func, defaultScopes);
 
-	  	expect(() => { 
-	  		wrappedFunc(1, invalidScope);
-	  	}).to.throw();
-	  });
+      expect(() => {
+        wrappedFunc(1, invalidScope);
+      }).to.throw();
+    });
 
-	  it('should throw error if scope argument is empty array', () => {
-	  	const func = spy();
-	  	const wrappedFunc = injectScopeValidator(func, defaultScopes);
+    it('should throw error if scope argument is empty array', () => {
+      const func = spy();
+      const wrappedFunc = injectScopeValidator(func, defaultScopes);
 
-	  	expect(() => { 
-	  		wrappedFunc(1, []);
-	  	}).to.throw();
-	  });
+      expect(() => {
+        wrappedFunc(1, []);
+      }).to.throw();
+    });
 
-	  it('should not throw error if scope argument is all but it had not been provided', () => {
-	  	const func = spy();
-	  	const wrappedFunc = injectScopeValidator(func, defaultScopes);
+    it('should not throw error if scope argument is all but it had not been provided', () => {
+      const func = spy();
+      const wrappedFunc = injectScopeValidator(func, defaultScopes);
 
-	  	expect(() => { 
-	  		wrappedFunc(1, ['all']);
-	  	}).to.not.throw();
-	  });
-	});
+      expect(() => {
+        wrappedFunc(1, allScope);
+      }).to.not.throw();
+    });
+  });
 
-	describe('scope argument as a string', () => {
-		it('should noop if scope argument is valid', () => {
-	  	const func = spy();
-	    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+  describe('scope argument as a string', () => {
+    it('should noop if scope argument is valid', () => {
+      const func = spy();
+      const wrappedFunc = injectScopeValidator(func, defaultScopes);
 
-	    expect(() => { 
-	    	wrappedFunc(1, validScope[0]);
-	    }).to.not.throw();
-	  });
+      expect(() => {
+        wrappedFunc(1, validScope[0]);
+      }).to.not.throw();
+    });
 
-	  it('should throw error if scope argument is unsupported', () => {
-	  	const func = spy();
-	    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+    it('should throw error if scope argument is unsupported', () => {
+      const func = spy();
+      const wrappedFunc = injectScopeValidator(func, defaultScopes);
 
-	    expect(() => { 
-	    	wrappedFunc(1, invalidScope[0]);
-	    }).to.throw();
-	  });	  
+      expect(() => {
+        wrappedFunc(1, invalidScope[0]);
+      }).to.throw();
+    });
 
-	  it('should throw error if empty string is provided as a scope', () => {
-	  	const func = spy();
-	    const wrappedFunc = injectScopeValidator(func, defaultScopes);
+    it('should throw error if empty string is provided as a scope', () => {
+      const func = spy();
+      const wrappedFunc = injectScopeValidator(func, defaultScopes);
 
-	    expect(() => { 
-	    	wrappedFunc(1, '');
-	    }).to.throw();
-	  });
-	});
+      expect(() => {
+        wrappedFunc(1, '');
+      }).to.throw();
+    });
+  });
 
-	it('should throw error if scope argument is not an array or string', () => {
-  	const func = spy();
+  it('should throw error if scope argument is not an array or string', () => {
+    const func = spy();
     const wrappedFunc = injectScopeValidator(func, defaultScopes);
 
-    expect(() => { 
-    	wrappedFunc(1, new Object({}));
+    expect(() => {
+      /* eslint-disable no-new-object */
+      wrappedFunc(1, new Object({}));
+      /* eslint-enable no-new-object */
     }).to.throw(TypeError);
   });
 });

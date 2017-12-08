2531f60a8765ca8776a2e717ac09aed1d42aa401
Buğra Ekuklu <ekuklu@icloud.com>
2017-12-08 14:14:57 +0300

    Added ESLint exclusion lines.

--- src/utils/getCurrentEnvironment.js
+++ src/utils/getCurrentEnvironment.js
@@ -1,19 +1,21 @@
 export default function getCurrentEnvironment() {
-	const isNode = new Function("try {return this===global;}catch(e){return false;}")();
+  /* eslint-disable no-new-func */
+  const isNode = new Function('try {return this===global;}catch(e){return false;}')();
+  /* eslint-enable no-new-func */
 
-	/* istanbul ignore else */
-	if (isNode) {
-		return {
-			name: 'node',
-			platform: process.platform,
-			pid: process.pid,
-			version: process.version,
-		};
-	}
+  /* istanbul ignore else */
+  if (isNode) {
+    return {
+      name: 'node',
+      platform: process.platform,
+      pid: process.pid,
+      version: process.version,
+    };
+  }
 
-	/* istanbul ignore next */
-	return {
-		name: 'browser',
-		userAgent: navigator.userAgent,
-	};
+  /* istanbul ignore next */
+  return {
+    name: 'browser',
+    userAgent: navigator.userAgent,
+  };
 }
--- tests/unit/utils/getCurrentEnvironment.js
+++ tests/unit/utils/getCurrentEnvironment.js
@@ -1,12 +1,12 @@
 import getCurrentEnvironment from 'src/utils/getCurrentEnvironment';
 
 describe('getCurrentEnvironment', () => {
-	it('should return environment as node', () => {
-		const env = getCurrentEnvironment();
+  it('should return environment as node', () => {
+    const env = getCurrentEnvironment();
 
-		expect(env.name).to.be.equal('node');
-		expect(env.platform).to.exist;
-		expect(env.pid).to.exist;
-		expect(env.version).to.exist;
-	});
+    expect(env.name).to.be.equal('node');
+    expect(env.platform).to.exist;
+    expect(env.pid).to.exist;
+    expect(env.version).to.exist;
+  });
 });
